91dcbdea0601191a335ed08b6f9552dcaf73a05b
vhfba <barbosav376@gmail.com>
2026-06-17 18:14:40 +0100

    Pen Drive update

--- copiarAmbienteTrabalho.ps1
+++ copiarAmbienteTrabalho.ps1
@@ -1,29 +1,30 @@
 # Verifies if Pen Drive if connected (or if folder exists)
-if(Test-Path -Path "E:\")
+if(Test-Path -Path "D:\")
 {
     try
     {
         $folder = Get-Date -Format Copia-"dd-MM-yyyy" # Create folder name with current date
         $sourceFolder = "$env:USERPROFILE\Desktop"
-        $destinationFolder = "E:\Copias\"
-        if (-not (Test-Path -Path $destinationFolder)) {
+        $destinationFolder = "D:\Copias\"
+        if (-not (Test-Path -Path $destinationFolder))
+        {
             New-Item -ItemType Directory -Path "$destinationFolder" -ErrorAction Stop
         }
 
         # Get all files (recursively)
-        $files = Get-ChildItem -Path $sourceFolder -Recurse -File
+        $files = Get-ChildItem -Path $sourceFolder -Recurse
         $total = $files.Count
         $count = 0
 
         New-Item -ItemType Directory -Path "$destinationFolder$folder" -ErrorAction Stop # Create folder in the Pen Drive (or any other folder)
 
-        foreach ($file in $files) {
+        foreach ($file in $files) 
+        {
             $relativePath = $file.FullName.Substring($sourceFolder.Length)
-            $targetPath   = Join-Path $destinationFolder $relativePath
+            $targetPath   = Join-Path $destinationFolder$folder $relativePath
 
             # Copy the file
             Copy-Item -Path $file.FullName -Destination $targetPath -Force
-
             $count++
             $percent = ($count / $total) * 100
 
@@ -31,7 +32,7 @@ if(Test-Path -Path "E:\")
                         -Status "Copying $($file.Name) ($count of $total)" `
                         -PercentComplete $percent
         }
-        Write-Progress -Activity "Copying files..." -Completed -Status "Sucesso!" -ForegroundColor green
+        Write-Progress -Activity "Copying files..." -Completed -Status "Sucesso!"
     }
     catch
     {
