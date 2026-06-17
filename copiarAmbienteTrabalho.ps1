fc58fe764219d9f914b6395f89c4d4485743e22b
vhfba <barbosav376@gmail.com>
2026-06-17 16:13:09 +0100

    Source path updated and progress bar added

--- copiarAmbienteTrabalho.ps1
+++ copiarAmbienteTrabalho.ps1
@@ -1,15 +1,37 @@
 # Verifies if Pen Drive if connected (or if folder exists)
-$deviceId = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid}
-if($null -ne $deviceId)
+if(Test-Path -Path "E:\")
 {
     try
     {
         $folder = Get-Date -Format Copia-"dd-MM-yyyy" # Create folder name with current date
         $sourceFolder = "$env:USERPROFILE\Desktop"
-        $destinationFolder = "$deviceId\Copias\"
+        $destinationFolder = "E:\Copias\"
+        if (-not (Test-Path -Path $destinationFolder)) {
+            New-Item -ItemType Directory -Path "$destinationFolder" -ErrorAction Stop
+        }
+
+        # Get all files (recursively)
+        $files = Get-ChildItem -Path $sourceFolder -Recurse -File
+        $total = $files.Count
+        $count = 0
+
         New-Item -ItemType Directory -Path "$destinationFolder$folder" -ErrorAction Stop # Create folder in the Pen Drive (or any other folder)
-        Copy-Item -Path "$sourceFolder\*" -Destination "$destinationFolder$folder" -Recurse -ErrorAction Stop # Copies everything in the source folder to the destination one 
-        Write-Host "Sucesso!" -ForegroundColor green
+
+        foreach ($file in $files) {
+            $relativePath = $file.FullName.Substring($sourceFolder.Length)
+            $targetPath   = Join-Path $destinationFolder $relativePath
+
+            # Copy the file
+            Copy-Item -Path $file.FullName -Destination $targetPath -Force
+
+            $count++
+            $percent = ($count / $total) * 100
+
+            Write-Progress -Activity "Copying files..." `
+                        -Status "Copying $($file.Name) ($count of $total)" `
+                        -PercentComplete $percent
+        }
+        Write-Progress -Activity "Copying files..." -Completed -Status "Sucesso!" -ForegroundColor green
     }
     catch
     {
@@ -18,5 +40,5 @@ if($null -ne $deviceId)
 } 
 else 
 {
-    Write-Host "Nenhuma PEN USB detectada!" -ForegroundColor red -BackgroundColor white # Error message
+    Write-Host "Disco não conectado!" -ForegroundColor red -BackgroundColor white # Error message
 }
